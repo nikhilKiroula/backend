e76156be28e2ee79b0fdb84a631607b8712f98d0
Nikhil Singh Kiroula <nikhilkiroula7@gmail.com>
2026-08-07 18:26:14 +0530

    utils code setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+// ? commonjs syntax
 // require('dotenv').config({path:"./env"})
+
+// ? modular js syntax 1
 import "dotenv/config";
+
+// ? modular js syntax 2
 // import dotenv from "dotenv"
 // dotenv.config({
 //     path: "./.env"
@@ -10,6 +15,20 @@ import { DB_NAME } from "./constants.js"
 import connectDB from "./db/index.js"
 
 connectDB()
+    .then(() => {
+
+        app.on("error", (error) => {
+            console.log("ERROR", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB connection failed!!", err);
+    })
 
 
 
