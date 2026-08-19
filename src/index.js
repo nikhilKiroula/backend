db7338f62c1c59b6f68dd5a7a56b0000c49696aa
Nikhil Singh Kiroula <nikhilkiroula7@gmail.com>
2026-08-19 20:57:28 +0530

    refactor: clean up unused code and comments in app.js, index.js, multer.middleware.js, and asyncHandler.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@
 // ? modular js syntax 1
 import "dotenv/config";
 
-// ? modular js syntax 2
-// import dotenv from "dotenv"
-// dotenv.config({
-//     path: "./.env"
-// })
-
 import mongoose from "mongoose"
 import { DB_NAME } from "./constants.js"
 import connectDB from "./db/index.js"
@@ -30,42 +24,3 @@ connectDB()
     .catch((err) => {
         console.log("MONGODB connection failed!!", err);
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import express from "express"
-// const app = express()
-
-//     (async () => {
-//         try {
-//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-
-//             app.on("error", (error) => {
-//                 console.log("ERROR : ", error);
-//                 throw error
-
-//             })
-
-//             app.listen(process.env.PORT, () => {
-//                 console.log(`App is listening on port ${process.env.PORT}`);
-
-//             })
-
-//         } catch (error) {
-//             console.log(`ERROR : ${error}`);
-//             throw error
-
-//         }
-//     })()
--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,17 +1,3 @@
-// import multer from "multer";
-
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, "./public/temp");
-//     },
-
-//     filename: (req, file, cb) => {
-//         cb(null, `${Date.now()}-${file.originalname}`);
-//     }
-// });
-
-// const upload = multer({ storage });
-
 import multer from "multer";
 
 const storage = multer.diskStorage({
